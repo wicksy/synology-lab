51b20b2552c41efd1b83dc369a17283aad2b1ad6
Martin Wicks <wicksy@wicksy.com>
2016-01-25 04:23:45 +0000

    Remove -it options from docker run to stop it failing in batch

--- filebotme.sh
+++ filebotme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 cd /tmp
-docker run -it -v /volume1/Downloads/:/tmp/Downloads/ wicksy/synology:latest bash -x /tmp/Downloads/bin/synctoS3.sh > /tmp/synctos3.log 2>&1
+docker run -v /volume1/Downloads/:/tmp/Downloads/ wicksy/synology:latest bash -x /tmp/Downloads/bin/synctoS3.sh > /tmp/synctos3.log 2>&1
 chmod 600 /tmp/synctos3.log
 /usr/local/bin/filebot -script 'fn:amc' /volume1/Downloads/ --output '/volume1/Media/Not Watched Yet' --action move -non-strict --conflict auto --lang en --def 'music=y' 'unsorted=y' 'clean=y' 'deleteAfterExtract=y' 'excludeList=.excludes' --log info --log-file '/volume3/@appstore/filebot-node/filebot.log' >> '/volume3/@appstore/filebot-node/log/1441471372245.log' 2>&1
 exit 0
