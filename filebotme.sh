fec8f4c3917b181a4749e2b1b0ebbe446c9df4d0
Martin Wicks <wicksy@wicksy.com>
2016-10-02 07:19:48 +0100

    Clean up duplicate files post-filebotme to avoid processing loop

--- filebotme.sh
+++ filebotme.sh
@@ -1,12 +1,18 @@
 #!/bin/sh -x
 
+# Email info
+#
 addressees="user@email.com anotheruser@email.com"
 subject="Subject of email here"
 
+# Copy selected files to S3 bucket
+#
 cd /tmp
 docker run --env-file=/usr/local/etc/synctoS3.env --volume=/volume1/Downloads/:/tmp/Downloads/ wicksy/synology:latest /scripts/synology-task-wrapper.py > /tmp/synctos3.log 2>&1
 chmod 600 /tmp/synctos3.log
 
+# If something has been uploaded send an email about it
+#
 if [[ -s /volume1/Downloads/EMAIL ]] ; then
   content="$(cat /volume1/Downloads/EMAIL)"
   for emailto in ${addressees}
@@ -17,17 +23,32 @@ if [[ -s /volume1/Downloads/EMAIL ]] ; then
   rm -f /volume1/Downloads/EMAIL
 fi
 
+# Tidy up old docker containers (that have exited)
+#
 for container in $(docker ps -a | egrep -v '(second|minute|hour)' | awk '/wicksy\/synology:/ {print $1}')
 do
   echo docker rm -fv ${container}
   docker rm -fv ${container}
 done
 
+# Tidy up orphans
+#
 for dangling in $(docker images -q -f "dangling=true")
 do
   docker rmi -f ${dangling}
 done
 
+# Move any video files to proper named folders, rename episodes, etc
+#
 /usr/local/bin/filebot -script 'fn:amc' /volume1/Downloads/ --output '/volume1/Media/Not Watched Yet' --action move -non-strict --conflict auto --lang en --def 'music=y' 'unsorted=y' 'clean=y' 'deleteAfterExtract=y' 'excludeList=.excludes' --log info --log-file '/volume3/@appstore/filebot-node/filebot.log' >> '/volume3/@appstore/filebot-node/log/1441471372245.log' 2>&1
+
+# Anything left move away so this script keeps trying to process it during subsequent calls
+#
+for unmoved in $(find /volume1/Downloads/ -type f -name '*.mp4' -o -name '*.avi' -o -name '*.mkv')
+do
+  echo ${unmoved}
+  mv -f "${unmoved}" "/volume1/Media/Not Watched Yet/Unsorted/"
+  rmdir "$(dirname ${unmoved})"
+done
 
 exit 0
